4629927f154e7f016708525e45170547a792c069
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-17 11:48:26 +0800

    test against dup resources

--- spec/defines/init_spec.rb
+++ spec/defines/init_spec.rb
@@ -20,4 +20,24 @@ describe 'easy_install', :type => :define do
     it { should compile }
   end
 
+  context 'two instances' do
+    let :pre_condition do
+      'include download_and_do
+      easy_install { "otherapp":
+        media_source => "http://www.coolapp.com/otherinstaller.tar",
+      }'
+    end
+    let :title do "coolapp" end
+    let :params do
+      {
+        :media_source => "http://www.coolapp.com/silentinstaller.tar",
+      }
+    end
+    context 'compiles ok' do
+      it { should compile }
+    end
+
+
+  end
+
 end
